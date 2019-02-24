d4b0a05b3c7cce58780ee56d2d4820a269d59171
iamgide555 <gittipat.1997@hotmail.com>
2019-02-24 18:37:35 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,35 @@
+from flask import Flask, redirect, url_for, request, render_template
+
+check = 0
+
+data = {
+    "firstNote": None,
+    "mood": None
+}
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html",check = check)
+
+@app.route('/check')
+def check(data):
+    return 
+
+@app.route('/genSong',methods = ['POST', 'GET'])
+def genSong():
+    if request.method == 'POST':
+        firstNote = request.form['firstNote']
+        if firstNote not in ["A","B","C","D","E","F","G"]:
+            check = 1
+            return render_template("index.html",check = check)
+        mood = request.form['mood']
+        return render_template("check.html",firstNote = firstNote,mood = mood)
+    else:
+        firstNote = request.args.get('firstNote')
+        mood = request.args.get('mood')
+        return url_for('check',data = {"FirstNote": firstNote, "Mood": mood})
+
+if __name__ == '__main__':
+   app.run(debug = True)
