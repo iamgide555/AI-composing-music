2c7af5b3ef28dffa8c0b9b63ef18a2c9cd80f814
iamgide555 <gittipat.1997@hotmail.com>
2019-02-26 14:34:20 +0700

    add load model

--- run.py
+++ run.py
@@ -1,11 +1,23 @@
 from flask import Flask, redirect, url_for, request, render_template
-
+from keras.models import load_model
+from keras import backend
 check = 0
 
 data = {
     "firstNote": None,
     "mood": None
 }
+
+# Init perplexity function
+def perplexity(y_true, y_pred):
+    cross_entropy = backend.categorical_crossentropy(y_true, y_pred)
+    perplexity = backend.pow(2.0, cross_entropy)
+    return perplexity
+
+def loadModel():
+    Hmodel = load_model('Fmodel_happy.h5',custom_objects={'perplexity': perplexity})
+    Smodel = load_model('Fmodel_sad.h5',custom_objects={'perplexity': perplexity})
+    Rmodel = load_model('Fmodel_relax.h5',custom_objects={'perplexity': perplexity})
 
 
 app = Flask(__name__)
@@ -22,6 +34,7 @@ def genSong():
             check = 1
             return render_template("index.html",check = check)
         mood = request.form['mood']
+
         return render_template("check.html",firstNote = firstNote,mood = mood)
     else:
         firstNote = request.args.get('firstNote')
@@ -30,4 +43,5 @@ def genSong():
         
 if __name__ == '__main__':
     print("Server running!!")
+    loadModel()
     app.run(debug = True)
