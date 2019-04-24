6e81790311a9db334e097568e1a04fffdc311aae
unknown <boy-zaza-@hotmail.com>
2019-04-24 19:18:04 +0700

    change nav bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,44 +1,19 @@
 <template>
   <div id="app">
-      <section class="hero is-dark">
-        <nav class="navbar is-transparent"  role="navigation" aria-label="main navigation">
-          <router-link to="/">
-            <div class="navbar-brand">
-              <a class="navbar-item" href="https://bulma.io">
-                <img src="http://pluspng.com/img-png/music-png-transparent-image-2400.png">
-                <H1 class="title">AI</H1>
-              </a>
-
-              <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-              </a>
+      <section>
+        <nav>
+          <div class="topnav">
+            <div class="logo">
+              <router-link to="/">
+                <a>Simple music</a>
+              </router-link>
             </div>
-          </router-link>
-
-          <div id="navbarBasicExample" class="navbar-menu">
-            <div class="navbar-end">
-              <div class="navbar-item">
-                <div class="buttons">
-                  <router-link to="/signup" v-if="!user">
-                    <a class="button">
-                      <strong>Sign up</strong>
-                    </a>
-                  </router-link>
-                  <router-link to="/login" v-if="!user">
-                    <a class="button is-primary">
-                      Log in
-                    </a>
-                  </router-link>
-                  <a
-                  v-else
-                  class="button is-success is-outlined"
-                  @click="logout()"
-                >Hi, {{user.username}} (Logout)</a>
-                </div>
-              </div>
-            </div>
+            <router-link to="/signup" v-if="!user">
+              <a>Sign up</a>
+            </router-link>
+            <router-link to="/login" v-if="!user">
+              <a>Login</a>
+            </router-link>
           </div>
         </nav>
       </section>
@@ -66,16 +41,45 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50; 
 }
+
 html,body {
   height: 100%;
   margin: 0;
   padding: 0;
   background-image: linear-gradient(#24234D,#241428);
+}
+.topnav {
+  background-color: rgba(4, 4, 39, 0.527);
+  overflow: hidden;
+}
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: right;
+  color: #fff0f0;
+  text-align: center;
+  padding: 8px 12px;
+  font-size: 18px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+.logo {
+  width: 13%;
+  float: left;
+  text-transform: uppercase;
+  color: #fff;
+  font-size: 15px;
+  text-align: left;
+  padding-left: 0px;
 }
 </style>
