f394fcab92919d2c3d0f5fec7b6d7afe3215827f
iamgide555 <gittipat.1997@hotmail.com>
2019-05-21 15:51:26 +0700

    fix stop logo

--- frontend/src/components/PlaySound.vue
+++ frontend/src/components/PlaySound.vue
@@ -22,7 +22,7 @@
                 <span class="total-time">0:00</span>
                 </div>
 
-                <div class="volume">
+                <div class="volume" >
                 <div class="volume-btn">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                     <path fill="#566574" fill-rule="evenodd" d="M14.667 0v2.747c3.853 1.146 6.666 4.72 6.666 8.946 0 4.227-2.813 7.787-6.666 8.934v2.76C20 22.173 24 17.4 24 11.693 24 5.987 20 1.213 14.667 0zM18 11.693c0-2.36-1.333-4.386-3.333-5.373v10.707c2-.947 3.333-2.987 3.333-5.334zm-18-4v8h5.333L12 22.36V1.027L5.333 7.693H0z" id="speaker"/>
@@ -176,7 +176,7 @@ export default {
         }
     function togglePlay() {
             if (player.paused) {
-                playPause.attributes.d.value = "M0 0h6v24H0zM12 0h6v24h-6z";
+                playPause.attributes.d.value = "M0 0h100 100v100 100L0 24V100";
                 player.play();
             } else {
                 playPause.attributes.d.value = "M18 12L0 24V0";
